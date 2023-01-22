aa31e82097f492fcec785fee0c305fc062482b05
Isaac Cherry <cherryisaac739@yahoo.com>
2023-01-22 07:33:02 -0500

    Code clean up + small changes

--- src/Book.js
+++ src/Book.js
@@ -1,31 +1,31 @@
-
 import { useState } from "react";
 import AddBook from "./AddBook";
-import React from 'react'
-
+import React from 'react';
 
 function Book() {
-//Add Book to List 
- const Button = ({color, text, onClick}) => {
+
+  const Button = ({ color, text, onClick }) => {
+    
   return (<button onClick={onClick} 
     style={{backgroundColor: color}} 
-    className="btn">{text} </button>)
+    className={`btn ${isDark ? 'dark' : 'light'}`}  >{text} </button>)
 }  
   const Header = ({ onAdd, showAdd }) => {
   return (
-    <header className='header'>
-        <Button color= {showAdd ? 'black' : 'green'} text = {showAdd ? 'Close' : 'Add Book to List'} onClick = {onAdd}/>
+    <header className='header'  >
+        <Button  color= {showAdd ? 'black' : 'green'} text = {showAdd ? 'Close' : 'Add Book to List'} onClick = {onAdd}/>
     </header>
   )
 }
 
 //Theme Button
 const [theme, setTheme] = useState("Dark Theme");
+//Prompt to change theme
 const [isDark, setIsDark] = useState(true);
 
 const handleThemeChange = () => {
   setIsDark(!isDark);
-  setTheme(theme === "Dark Theme" ? "Light Theme" : "Dark Theme");
+  // setTheme(theme === 'Dark Theme' ? 'Light Theme' : 'Dark Theme');
 }
 
   //Hide book add by default (useState set to false)
@@ -34,8 +34,8 @@ const handleThemeChange = () => {
   //For changing state such as deleting a book
   const [book, setBook] = useState([{
   id: 1,
-  //https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlkk-L1KiG7ca0hK9_uEefpx3f5KhI2E4h7w&usqp=CAU
-  img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL._AC_UL604_SR604,400_.jpg',
+  img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlkk-L1KiG7ca0hK9_uEefpx3f5KhI2E4h7w&usqp=CAU',
+  // img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL._AC_UL604_SR604,400_.jpg',
   title: 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
   author: 'James Clear',
   description: 'Atomic Habits (2018) provides a practical and proven framework for creating good habits and shedding bad ones. Drawing on scientific research and real-life examples, it shows how tiny changes in behavior can result in the formation of new habits and help you achieve big things.'
@@ -48,21 +48,23 @@ const handleThemeChange = () => {
 },
  {
    id: 3,
-  img: 'https://images-na.ssl-images-amazon.com/images/I/619Gn-+VNQL._AC_UL302_SR302,200_.jpg',
+  // img: 'https://images-na.ssl-images-amazon.com/images/I/619Gn-+VNQL._AC_UL302_SR302,200_.jpg',
+   img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1347752011l/12469670.jpg',
   title: 'How to Win Friends & Influence People',
   author: 'Dale Carnegie',
-  description: 'This book teaches you countless principles to become a likable person, handle your relationships well, win others over and help them change their behavior without being intrusive.'
+  // description: 'This book teaches you countless principles to become a likable person, handle your relationships well, win others over and help them change their behavior without being intrusive.'
+   description: 'In this book summary on How to Win Friends and Influence People in the Digital Age, we’ll review ideas from the original book, with tips on how to apply them in today’s digital age, so you can stand out as a leader and build rich, trusting and lasting relationships.'
 }
 ])
 
+//Main Bookcase background change...not sure how to make it work correctly
 const [backgroundImage, setBackgroundImage] = useState("image.png");
 
-const changeBackground = () => {
-  setBackgroundImage("stars-galaxy.gif");
-}
-
-const imagePath = !isDark ? "../src/Images/stars-galaxy.gif" : 
-".../src/Imagesimage.png";
+  function handleChangeBackground() {
+    setIsDark(!isDark);
+    setBackgroundImage(backgroundImage ===
+      '../src/Images/starts-galaxy.gif' ? '../src/Images/images.png' : '../src/Images/starts-galaxy.gif');
+  }
 
 //To delete a book
   const removeItem = (id) => {
@@ -85,7 +87,7 @@ const addBookkId = (someBook) => {
   }
   
   return (
-  <article className={`book ${isDark ? 'dark' : 'light'}`}>
+  <article className={`book ${isDark ? 'dark' : 'light'}`} >
     <Header onAdd={() => setShowBook(!showBook)}
       showAdd={showBook} />
       {showBook && <AddBook onAdd = {addBookkId} />}
@@ -107,13 +109,14 @@ const addBookkId = (someBook) => {
                 {descriptionVisible ? <p className="description flip2" style={{color: !isDark ? "rgb(0, 255, 255, 0.9)" : "black", fontWeight: !isDark ? 'bold' : "inherit",
                 borderColor: !isDark ? "white" : "black", backgroundColor: !isDark ? "rgb(0, 0, 0, 0.7)" : "rgb(255, 255, 255, 0.8)"}} 
                 onClick={()=>toggleBook(id)}>{description}</p> :
-                 <img className="flip" src={img}  onClick={()=> toggleBook(id)} alt="" />}
+                    <img className="flip" src={img} onClick={() => toggleBook(id)} alt="" style={{
+                      color: !isDark ? "dark" : "light"}} />}
                 <h1 style={{color: !isDark ? "white" : "black", textShadow: !isDark ? "0 0 3px #FF0000, 0 0 5px #0000FF" : ""}}>{title}</h1>
                 <h3 style={{color: !isDark ? "coral" : "black", fontFamily: !isDark ? "fantasy" : "",
               textShadow: !isDark ? "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black" : ""}}>{author}</h3>
                 <h1>{}</h1>
                  {/* {descriptionVisible && <h1 className="description"><span >{booking.description}</span></h1>} */}
-                    <button className="bttn"
+                  <button className={`bttn ${isDark ? 'dark' : 'light'}`} 
                         onClick={() => removeItem(id)}>
                         Delete
                     </button>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import ReactDOM from 'react-dom'
+import { useState } from "react";
 import './index.css'
 import Book from './Book'
 //CTRL + SHIFT + P to open simple browser
 //My-Book-List-react.js
 
 function BookList() {
+
   return (
+    <body className='contain'>
     <section className='booklist'>
-     <Book />
+        <Book />
     </section>
+    </body>
   )
 } 
-
-BookList.defaultProps = {
-  color: 'orange'
-}
 
 ReactDOM.render(<BookList/>, document.getElementById('root'))
