22c9718fe0065e997d3ef5abbb017925998a280a
Isaac <97069528+cherryisaac@users.noreply.github.com>
2023-01-27 07:46:43 -0500

    Delete ToggleBook.js

--- src/ToggleBook.js
+++
@@ -1,8 +0,0 @@
-import { FaTimes } from 'react-icons/fa'
-
-const ToggleBook = ({ book, onToggle }) => {
-    return (
-        <div className= {`task ${book.reminder ? 'reminder' : ''}`}
-        > {book.text}</div>
-    )
-}
